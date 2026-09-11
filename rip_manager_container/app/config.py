3607f7271302d9846817fe739e578e6aa4b5b94b
adamrfreeman644 <adamfreeman644@gmail.com>
2026-09-11 16:55:39 +0100

    Set Rip Manager app version to 0.20.8

--- rip_manager_container/app/config.py
+++ rip_manager_container/app/config.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-VERSION = "0.20.7"
+VERSION = "0.20.8"
 
 DATA_DIR = Path(os.getenv("RIP_MANAGER_DATA", "/data"))
 CONFIG_DIR = Path(os.getenv("RIP_MANAGER_CONFIG", "/config"))
