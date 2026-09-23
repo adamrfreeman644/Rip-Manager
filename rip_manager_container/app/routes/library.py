71ed4502061864d55a49515949501d8808852c4b
adamrfreeman644 <adamfreeman644@gmail.com>
2026-09-23 18:32:34 +0100

    Avoid browser blocking Library API requests (v0.22.29)

--- rip_manager_container/app/routes/library.py
+++ rip_manager_container/app/routes/library.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import library
 import upc
 
-router=APIRouter(prefix="/api/library",tags=["library"])
+router=APIRouter(prefix="/api/collection",tags=["library"])
 
 class BulkImport(BaseModel):
     upcs: str
